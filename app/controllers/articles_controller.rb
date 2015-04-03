ed3d076d01afc1a2c63eb8ff88b1f2d7bbc23306
Ryoto Hotta <grapevinechip@gmail.com>
2015-04-04 07:23:18 +0900

    view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,15 +29,13 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.user = current_user
     if signed_in?
-      if @article.save
-        client = Twitter::REST::Client.new do |config|
-          config.consumer_key         = "Qndxjz83ZuZHwm7LoN9VX0wzC"
-          config.consumer_secret      = "BmrtnW4g97au2Zn3TFPZKH09oyZjFOIOCmco0j7PY5JUikk43e"
-          config.access_token         = session[:oauth_token]
-          config.access_token_secret  = session[:oauth_token_secret]
-        end
-        client.update("zangeをして、chigiriを結びました。zange.co")
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key         = "Qndxjz83ZuZHwm7LoN9VX0wzC"
+        config.consumer_secret      = "BmrtnW4g97au2Zn3TFPZKH09oyZjFOIOCmco0j7PY5JUikk43e"
+        config.access_token         = session[:oauth_token]
+        config.access_token_secret  = session[:oauth_token_secret]
       end
+      client.update("Zangeをして、Chigiriを結びました。zange.co #zange")
     end
 
     respond_to do |format|
