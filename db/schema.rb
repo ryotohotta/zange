07b9fc98a9bd17504201a9c5745de8c87e99c5e7
Ryoto Hotta <grapevinechip@gmail.com>
2015-04-03 15:08:25 +0900

    Revert "Revert a2ae3a4..63b00e2"
    This reverts commit 5a4b9dec3498bd83a8993fc7c0778fc6c87bf7f3.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326045534) do
+ActiveRecord::Schema.define(version: 20150321065627) do
 
   create_table "articles", force: :cascade do |t|
     t.integer  "user_id"
